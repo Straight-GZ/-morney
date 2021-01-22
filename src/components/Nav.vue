f5568c2aac14e298aa257c0b467e9d3e7e3fc224
Straight <gengzhi1993@163.com>
2021-01-22 17:20:07 +0800

    使用active-class

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,16 @@
 <template>
   <nav>
-    <router-link class='item' to="/money">
+    <router-link class='item' active-class="selected" to="/money">
       <Icons name="money"/>
       记账
     </router-link>
 
-    <router-link class='item' to="/labels">
+    <router-link class='item' active-class="selected" to="/labels">
       <Icons name="label"/>
       标签
     </router-link>
 
-    <router-link class="item" to="/statistics">
+    <router-link class="item" active-class="selected" to="/statistics">
       <Icons name="statistics"/>
       统计
     </router-link>
@@ -41,6 +41,9 @@ nav {
       width: 32px;
       height: 32px;
     }
+  }
+  >.item.selected{
+    color:red;
   }
 }
 </style>
