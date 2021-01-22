b3697d2149a91e8ebd107d597fd6e230ce72a13f
Straight <gengzhi1993@163.com>
2021-01-22 14:04:05 +0800

    使用 <Layout /> 组件

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,9 +1,8 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-      <p>Label.vue</p>
-    </div>
-    <Nav/>
+  <div>
+    <Layout>
+      <p>Labels.vue</p>
+    </Layout>
   </div>
 </template>
 
@@ -13,17 +12,3 @@ export default {
   name: "Labels",
 };
 </script>
-
-<style lang="scss" scoped>
-.nav-wrapper {
-  border: 1px solid black;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-
-.content {
-  flex-grow: 1;
-  overflow: auto;
-}
-</style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
+  <div>
+    <Layout>
       <p>Money.vue</p>
-    </div>
-    <Nav/>
+    </Layout>
   </div>
+
 </template>
 
 <script lang="ts">
@@ -12,18 +12,3 @@ export default {
   name: "Money",
 };
 </script>
-
-<style lang="scss" scoped>
-.nav-wrapper {
-  border: 1px solid black;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-
-.content {
-  flex-grow: 1;
-  overflow: auto;
-}
-
-</style>
