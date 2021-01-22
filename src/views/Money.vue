87a703d5f72430b2e489c3dbead6aef387479137
Straight <gengzhi1993@163.com>
2021-01-22 23:38:58 +0800

    添加 Money组件 HTML

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,10 +1,48 @@
 <template>
-  <div>
-    <Layout>
-      <p>Money.vue</p>
-    </Layout>
-  </div>
-
+  <Layout>
+    <div class="tags">
+      <ul class="current">
+        <li>衣</li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+      </ul>
+      <button class="new">添加标签</button>
+    </div>
+    <div>
+      <label class="notes">
+        <span>备注</span>
+        <input type="text">
+      </label></div>
+    <div class="types">
+      <button>支出</button>
+      <button>收入</button>
+    </div>
+    <div>
+      <div class="number"></div>
+      <div class="buttons">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>+</button>
+        <button>删除</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>
+        <button>清空</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>×</button>
+        <button>0</button>
+        <button>.</button>
+        <button>%</button>
+        <button>÷</button>
+        <button>OK</button>
+      </div>
+    </div>
+  </Layout>
 </template>
 
 <script lang="ts">
@@ -12,3 +50,7 @@ export default {
   name: "Money",
 };
 </script>
+<style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+
+</style>
