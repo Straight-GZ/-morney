db795a92d68a06f7c4fb32e6fbadc4f849612096
Straight <gengzhi1993@163.com>
2021-01-27 17:17:37 +0800

    创建 Button组件

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="$emit('click',$event)">
+    <slot/>
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+
+@Component
+export default class Button extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
