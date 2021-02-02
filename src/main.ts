96ba2f3891eaaf11a5cf4c2bcb00cd6d7c334dd5
Straight <gengzhi1993@163.com>
2021-02-02 23:28:55 +0800

    添加 二维码

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,23 @@ new Vue({
 
 window.onload = function () {
   setTimeout(function () {
-    window.scroll(0, 10000);
+    window.scroll(0, 200);
   }, 0);
 };
+
+if (document.documentElement.clientWidth > 500) {
+  window.alert('请使用手机打开，以保证浏览效果');
+  const img = document.createElement('img');
+  img.src = '/qrcode.png';
+  img.style.position = 'fixed';
+  img.style.left = '50%';
+  img.style.top = '50%';
+  img.style.transform = 'translate(-50%,-50%)';
+  img.style.boxShadow = '0,0,10px,rgba(0,0,0,0.25)';
+  document.body.appendChild(img);
+  const removeImg = () => {
+    document.body.removeChild(img);
+  };
+  document.body.addEventListener('click', removeImg);
+  document.body.removeEventListener('click', removeImg);
+}
